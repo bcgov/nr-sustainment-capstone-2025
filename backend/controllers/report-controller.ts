94bc494686320abba018769a74b54bd52bc3c98f
dallascrichmond <113049138+dallascrichmond@users.noreply.github.com>
2025-05-12 12:25:07 -0700

    Just a quick little tweak. Was missing an *

--- backend/controllers/report-controller.ts
+++ backend/controllers/report-controller.ts
@@ -1,10 +1,9 @@
 import { Response, Request } from 'express';
 
 /**
- *@summary  add report is the main adding report page and from there
-            we can add other reports ie - addSurfaceCoverageReport 
-  */ 
-
+ * @summary Add report is the main adding report page and from there
+ *          we can add other reports ie - addSurfaceCoverageReport 
+ */ 
 const addReport = (req: Request, res: Response) => {
   res.status(200).send('Add report is working');
 };
