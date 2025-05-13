10c78343d9fb153eb702226ad2e7a2b60ac52f06
janessacheng <janessacheng94@gmail.com>
2025-05-13 12:03:54 -0700

    fixed background image

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import Categories from './Categories.tsx'
+import Categories from './Categories'
+//import Categories from './Categories.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
