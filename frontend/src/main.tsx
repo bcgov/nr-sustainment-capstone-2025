26aa870fa1983f7ce4e4bac747b823cc61da6797
janessacheng <janessacheng94@gmail.com>
2025-05-13 14:34:31 -0700

    Replaced Categories for testing on main.tsx back to App

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import Categories from './Categories'
 //import Categories from './Categories.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Categories />
+    <App />
   </StrictMode>,
 )
