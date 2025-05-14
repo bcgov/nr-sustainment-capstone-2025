3509ab5bfa6a9e927881c4c2fe8df1120c4090eb
janessacheng <janessacheng94@gmail.com>
2025-05-14 13:02:56 -0700

    Corrected Soil Crusting text to Soil Coverage on Categories

--- frontend/src/components/Categories/Categories.tsx
+++ frontend/src/components/Categories/Categories.tsx
@@ -1,23 +1,20 @@
-import {useLocation} from 'react-router-dom';
 import Footer from '../common/Footer/Footer.tsx';
 import Header from '../common/Header/Header.tsx';
 import { Button } from '../common/Button/Button.tsx';
 import '../Categories/categories.styles.css';
 
 function Categories(){
-    const location = useLocation();
-
     return(
         <>
             <Header />
-                <h2>Pick a category for {location.state.page}:</h2>
+                <h2>Pick a category:</h2>
                 <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
                     <div>
                         <Button size={'lg'} variant='primary' disabled={false} text={'Ponding'} ></Button>
                         <Button size={'lg'} variant='primary' disabled={false} text={'Surface Crusting'} ></Button>
                     </div>
                     <div>
-                        <Button size={'lg'} variant='primary' disabled={false} text={'Soil Crusting'} ></Button>
+                        <Button size={'lg'} variant='primary' disabled={false} text={'Soil Coverage'} ></Button>
                         <Button size={'lg'} variant='primary' disabled={false} text={'Biospores'} ></Button>
                     </div>
                     <div>
