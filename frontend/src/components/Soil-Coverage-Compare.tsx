df5e8b4e561da1cb0b91f9c8fbab013bc9868eda
janessacheng <janessacheng94@gmail.com>
2025-05-16 13:47:49 -0700

    Added handleClick functionality to nav buttons

--- frontend/src/components/Soil-Coverage-Compare.tsx
+++ frontend/src/components/Soil-Coverage-Compare.tsx
@@ -1,15 +1,29 @@
 import Footer from './common/Footer/Footer.tsx';
 import Header from './common/Header/Header.tsx';
 import { Button } from './common/Button/Button.tsx';
+import { useNavigate } from 'react-router-dom';
 
 function CoverageCompare(){
+    const navigate = useNavigate();
+
+    const handleReturnHomeClick = () => {
+        navigate("/");
+    }
+
+    const handleCaptureDataClick = () => {
+        navigate("/categories", {state:{page:'capture'}});
+    }
+
+    const handleCompareDataClick = () => {
+        navigate("/categories", {state:{page:'compare'}});
+    }
     return(
         <>
             <Header />
                 <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                    <Button size={'md'} variant='secondary' disabled={false} text={'Back to Home'} />
-                    <Button size={'md'} variant='primary' disabled={false} text={'Input Another Category'} />
-                    <Button size={'md'} variant='primary' disabled={false} text={'Compare Data'} />
+                    <Button size={'md'} variant='secondary' disabled={false} text={'Back to Home'} handleClick={handleReturnHomeClick}/>
+                    <Button size={'md'} variant='primary' disabled={false} text={'Input Another Category'} handleClick={handleCaptureDataClick}/>
+                    <Button size={'md'} variant='primary' disabled={false} text={'Compare Data'} handleClick={handleCompareDataClick}/>
                 </div>
             <Footer />
         </>
