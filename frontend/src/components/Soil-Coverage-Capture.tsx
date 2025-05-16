dbad9472f6632b66e87f95a417c62adfc8df559e
Manny-Sandhu <manindersandhu@hotmail.ca>
2025-05-16 15:18:39 -0700

    added functionality to navigation buttons for soils coverage capture page

--- frontend/src/components/Soil-Coverage-Capture.tsx
+++ frontend/src/components/Soil-Coverage-Capture.tsx
@@ -2,6 +2,7 @@ import Footer from './common/Footer/Footer.tsx';
 import Header from './common/Header/Header.tsx';
 import { Button } from './common/Button/Button.tsx';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 
 /*
@@ -17,6 +18,20 @@ function SoilCoverageCapture(){
     };
     
 
+    const navigate = useNavigate();
+
+    const handleReturnHomeClick = () => {
+        navigate("/");
+    }
+
+    const handleCaptureDataClick = () => {
+        navigate("/categories", {state:{page:'capture'}});
+    }
+
+    const handleCompareDataClick = () => {
+        navigate("/categories", {state:{page:'compare'}});
+    }
+
     return(
         <>
             <Header />
@@ -30,9 +45,9 @@ function SoilCoverageCapture(){
                             />
                         </div>
                     )}
-                    <Button size={'md'} variant='secondary' disabled={false} text={'Back to Home'} />
-                    <Button size={'md'} variant='primary' disabled={false} text={'Input Another Category'} />
-                    <Button size={'md'} variant='primary' disabled={false} text={'Compare Data'} />
+                    <Button size={'md'} variant='secondary' disabled={false} text={'Back to Home'} handleClick={handleReturnHomeClick}/>
+                    <Button size={'md'} variant='primary' disabled={false} text={'Input Another Category'} handleClick={handleCaptureDataClick} />
+                    <Button size={'md'} variant='primary' disabled={false} text={'Compare Data'} handleClick={handleCompareDataClick}/>
                 </div>
             <Footer />
         </>
