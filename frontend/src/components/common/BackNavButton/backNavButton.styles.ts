8726b848f8c5e2529de2e69990494974d4f78bda
janessacheng <janessacheng94@gmail.com>
2025-05-27 11:46:53 -0700

    Adjusted position of  back button, no longer runs into header

--- frontend/src/components/common/BackNavButton/backNavButton.styles.ts
+++ frontend/src/components/common/BackNavButton/backNavButton.styles.ts
@@ -14,7 +14,7 @@ export const StyledBackButton = styled.button`
     color: #333;
     position: absolute;
     left: 5vh;
-    top: 10vh;
+    top: 11vh;
     align-items: center;
     justify-content: center;
     margin: 0;
