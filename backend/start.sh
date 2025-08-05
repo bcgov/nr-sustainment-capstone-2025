7509c9cbcf1cfc080362af9fe5d9ef955c72f286
Fergus MacConnell WLRS:EX <f.macconnell@gmail.com>
2025-08-05 11:27:58 -0700

    build database url.

--- backend/start.sh
+++ backend/start.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+export DATABASE_URL="postgresql://postgres:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:5432/postgres?schema=public"
+
 echo "Running Prisma migrate..."
 npx prisma migrate deploy
 
